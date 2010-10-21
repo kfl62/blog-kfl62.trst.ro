5f745a5a14e59ab91d149e0dd78725092ae2d235
kfl62 <kfl62g@gmail.com>
2010-10-21 19:57:02 +0300

    compass config

--- lib/compass.rb
+++ lib/compass.rb
@@ -2,7 +2,7 @@
 # named “config.yaml”.
 
 project_path = File.dirname(__FILE__)
-http_path    = '/'
-output_style = :compressed
-sass_dir     = 'content/stylesheets'
-css_dir      = 'output/stylesheets'
+http_path    = '../'
+#output_style = :compressed
+sass_dir     = '/content/stylesheets'
+css_dir      = '/output/stylesheets'
