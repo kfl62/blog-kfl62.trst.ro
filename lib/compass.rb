2fd61b14a1a5a0f3c1e1f3304f582d2305da2f05
kfl62 <kfl62g@gmail.com>
2010-10-25 22:16:56 +0300

    minimal compass config

--- lib/compass.rb
+++ lib/compass.rb
@@ -1,8 +1,7 @@
-# This is the Compass configuration file. The configuration file for nanoc is 
-# named “config.yaml”.
+# encoding: utf-8
+=begin
+Some description here
+=end
 
-project_path = File.dirname(__FILE__)
 http_path    = '../'
-#output_style = :compressed
-sass_dir     = '/src/stylesheets'
-css_dir      = '/public/stylesheets'
+
