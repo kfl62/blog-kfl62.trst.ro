2954a4ea6c44f0dc58a4d2e5d35e621698901105
kfl62 <kfl62g@gmail.com>
2010-10-26 21:04:22 +0300

    new compass config

--- lib/compass.rb
+++ lib/compass.rb
@@ -1,7 +1,12 @@
 # encoding: utf-8
 =begin
-Some description here
+This is common configuration
 =end
 
-http_path    = '../'
-
+sass_dir = File.join('content', 'stylesheets')
+images_dir = File.join('public', 'images')
+cache_dir = File.join('tmp','sass-cache')
+http_path = '../'
+http_images_path = '/images'
+http_stylesheets_path = '/stylesheets'
+sass_options = {:cache_location => 'tmp/sass-cache'}
